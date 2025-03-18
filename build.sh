3bfc56d6f32615e47ba7b8d760bd0960bdf9af7b
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2025-03-18 09:39:34 +0100

    fix: use docker build variables
    Signed-off-by: Umegbewe Nwebedu <nwebedujunior55@gmail.com>

--- build.sh
+++ build.sh
@@ -2,35 +2,17 @@
 
 set -euo
 
-HOME="$(cd "$(dirname "$0")" && pwd)"
+FABRIC_HOME="$(cd "$(dirname "$0")" && pwd)"
 TOOLS_IMAGE_BASE_NAME="ghcr.io/fablo-io/fabric-tools:3.0.0"
 
 build_and_push_tools() {
-    local platform
-    local arch
-
-    platform=$(uname -s | tr '[:upper:]' '[:lower:]')
-    case $(uname -m) in
-        x86_64)
-            arch="amd64"
-            ;;
-        aarch64|arm64)
-            arch="arm64"
-            ;;
-        *)
-            echo "Unsupported architecture: $(uname -m)"
-            exit 1
-            ;;
-    esac
-
     if [ "${1:-''}" = "--push" ]; then
-        docker build \
+        docker buildx build \
             --build-arg FABRIC_VERSION=3.0.0 \
-            --build-arg ARCH="$arch" \
-            --build-arg PLATFORM="$platform" \
+            --platform linux/amd64,linux/arm64 \
             --tag "$TOOLS_IMAGE_BASE_NAME" \
             --push \
-            "$HOME"
+            "$FABRIC_HOME"
     else
         docker build \
             --build-arg FABRIC_VERSION=3.0.0 \
